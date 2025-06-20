e8084d3925f255182526d28eecb467df5d9fe14d
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 05:27:11 +0000

    [automatic commit] 2025-06-20 05:27:11 UTC 27.33.121.122

--- part1/db.js
+++ part1/db.js
@@ -1,35 +1,10 @@
-var mysql = require('mysql2/promise');
-
-let pool;
-
-async function initDb() {
-  pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'DogWalkService'
-  });
-}
-
-function query(sql, params) {
-  if (!pool) {
-    throw new Error('Database pool not initialized');
-  }
-  return pool.query(sql, params);
-}
-
-module.exports = {
-  initDb,
-  query
-};
-
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'textbook_marketplace'
+    database: 'DogWalkService'
 });
 
 module.exports = db;
