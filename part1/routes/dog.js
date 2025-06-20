bf4339356e013030c58447d84c20244a2b6ea7e2
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 04:28:52 +0000

    [automatic commit] 2025-06-20 04:28:51 UTC 27.33.121.122

--- part1/routes/dog.js
+++ part1/routes/dog.js
@@ -7,8 +7,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/dogs', async function(req, res, next {
-
+router.get('/dogs', async function(req, res, next) {
+    try {
+    const [rows] = await pool.query(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving dogs' });
+  }
 });
 
 module.exports = router;
