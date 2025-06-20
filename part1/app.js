8f066435b09970dd56712043f744a5bc3929e7f2
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 04:05:26 +0000

    [automatic commit] 2025-06-20 04:05:26 UTC 27.33.121.122

--- part1/app.js
+++ part1/app.js
@@ -34,7 +34,7 @@ let db;
     });
 
     // Insert test data Users
-    await pool.query(`INSERT IGNOREMINTO Users (username, email, password_hash, role)
+    await pool.query(`INSERT IGNORE INTO Users (username, email, password_hash, role)
                 VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
                         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
                         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
