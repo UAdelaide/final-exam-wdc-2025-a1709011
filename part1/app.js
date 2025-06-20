e2a2754111e3a72a33ccd82fd990b7624825863d
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 04:20:02 +0000

    [automatic commit] 2025-06-20 04:20:02 UTC 27.33.121.122

--- part1/app.js
+++ part1/app.js
@@ -124,7 +124,7 @@ let db;
       `);
     }
   } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+    console.error('Error inserting test data:', err);
   }
 })();
 
