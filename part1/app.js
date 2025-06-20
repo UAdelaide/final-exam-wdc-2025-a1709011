68a3218abb50045545d51daf2160a96fc0ba7d1c
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 04:05:05 +0000

    [automatic commit] 2025-06-20 04:05:05 UTC 27.33.121.122

--- part1/app.js
+++ part1/app.js
@@ -33,8 +33,8 @@ let db;
       database: 'DogWalkService'
     });
 
-    // Insert test data
-    await pool.query(`INSERT INTO Users (username, email, password_hash, role)
+    // Insert test data Users
+    await pool.query(`INSERT IGNOREMINTO Users (username, email, password_hash, role)
                 VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
                         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
                         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
