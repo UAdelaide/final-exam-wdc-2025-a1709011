f6f6be8ce5694a89b62820b304f88cd87f3202f6
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 05:04:05 +0000

    [automatic commit] 2025-06-20 05:04:05 UTC 27.33.121.122

--- part1/app.js
+++ part1/app.js
@@ -94,31 +94,14 @@ async function insertTestData() {
 
 (async () => {
   try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: ''
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
-
-    // Now connect to the created database
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'DogWalkService'
-    });
-
-
-
-  } catch (err) {
-    console.error('Error inserting test data:', err);
+    await initDb();
+    console.log("Database connected.");
+    await insertTestData();
+  } catch (error) {
+    console.error("Error initializing database:", error);
   }
 })();
+
 
 
 app.use(logger('dev'));
