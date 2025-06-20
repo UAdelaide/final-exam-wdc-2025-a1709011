b7f14c5d1bbe8f07f44bcedaf82f122845ca4486
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 05:26:50 +0000

    [automatic commit] 2025-06-20 05:26:50 UTC 27.33.121.122

--- part1/db.js
+++ part1/db.js
@@ -23,4 +23,13 @@ module.exports = {
   query
 };
 
+const mysql = require('mysql2/promise');
 
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'textbook_marketplace'
+});
+
+module.exports = db;
