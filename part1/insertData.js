f5f2c0adbcc6a4b0e1b34002909cd28e38422b58
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 05:21:58 +0000

    [automatic commit] 2025-06-20 05:21:58 UTC 27.33.121.122

--- part1/insertData.js
+++ part1/insertData.js
@@ -1,1 +1,10 @@
 var mysql = require('mysql2/promise');
+
+async function initDb() {
+  pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'DogWalkService'
+  });
+}
