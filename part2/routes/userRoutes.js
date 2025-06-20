1a8a8403f2172d885048dd4b726ce0ce9c926533
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 11:00:16 +0000

    [automatic commit] 2025-06-20 11:00:16 UTC 27.33.121.122

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -87,7 +87,7 @@ router.post('/logout', (req, res) => {
   });
 });
 
-router.get('/api/dogs', async function(req, res, next) {
+router.get('/dogs', async function(req, res, next) {
     try {
         const [rows] = await db.query(`
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
