76f99b4a0f3fbc87d0b96a89fe0d48949a2a71d0
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 04:15:53 +0000

    [automatic commit] 2025-06-20 04:15:53 UTC 27.33.121.122

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,14 @@ let db;
     `);
 
     // WalkRequests
-    await db.query(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+    await db.query(`INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+                    SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open'
+                    FROM Dogs
+                    WHERE name = 'Max';
+
+    `);
+
+      await db.query(`INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
                     SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open'
                     FROM Dogs
                     WHERE name = 'Max';
