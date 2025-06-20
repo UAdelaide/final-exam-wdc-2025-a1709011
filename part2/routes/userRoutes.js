fe1a780e15216579d02b3ef340ce4b4d30cf1a07
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 09:03:06 +0000

    [automatic commit] 2025-06-20 09:03:05 UTC 27.33.121.122

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 const session = require('express-session');
+const mysql = require('mysql2/promise');
+
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
