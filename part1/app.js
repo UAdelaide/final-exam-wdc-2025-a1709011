1103550335ac57bf564de1e9ebebadea4cb92b98
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 04:18:38 +0000

    [automatic commit] 2025-06-20 04:18:38 UTC 27.33.121.122

--- part1/app.js
+++ part1/app.js
@@ -108,10 +108,9 @@ let db;
     `);
 
     await db.query(`INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-                    SELECT dog_id, '2025-06-05 09:45:00', 35, 'Adelaide', 'cancelled'
+                    SELECT dog_id, '2025-06-03 10:45:00', 55, 'Henly Beach', 'completed'
                     FROM Dogs
-                    WHERE name = 'Mel'
-
+                    WHERE name = 'Jackie'
     `);
 
     // Insert data if table is empty
