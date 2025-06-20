4f5d086bc030ab76aa1ed2ded5e1e8dc61e312e5
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 11:06:51 +0000

    [automatic commit] 2025-06-20 11:06:51 UTC 27.33.121.122

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -90,7 +90,7 @@ router.post('/logout', (req, res) => {
 router.get('/dogs', async function(req, res, next) {
     try {
         const [rows] = await db.query(`
-        SELECT d.name AS dog_name, d.size, u.username AS owner_username
+        SELECT d.dog_id, d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
         `);
