5b157f88535f9d9dcb1998be138cce6207f8efe9
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 12:12:43 +0000

    [automatic commit] 2025-06-20 12:12:43 UTC 27.33.121.122

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -87,7 +87,7 @@ router.post('/logout', (req, res) => {
   });
 });
 
-router.get('/dogs', async function(req, res, next) {
+router.get('/alldogs', async function(req, res, next) {
   if (!req.session || !req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
