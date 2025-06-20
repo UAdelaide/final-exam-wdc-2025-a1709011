d0f8784ae78b4ecd6e0395e8746c06c15d9405b7
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-20 04:09:15 +0000

    [automatic commit] 2025-06-20 04:09:15 UTC 27.33.121.122

--- part1/app.js
+++ part1/app.js
@@ -34,7 +34,7 @@ let db;
     });
 
     // Insert test data Users
-    await pool.query(`INSERT IGNORE INTO Users (username, email, password_hash, role)
+    await db.query(`INSERT IGNORE INTO Users (username, email, password_hash, role)
                 VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
                         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
                         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
@@ -43,7 +43,9 @@ let db;
     `);
 
     // Dogs
-    await db.query
+    await db.query(`
+
+    `);
 
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
